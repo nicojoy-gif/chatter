9d6f8da968c3a5fefed0c14f6f2a0de398187c43
Nicoratty <Nicoratty1@gmail.com>
2023-07-19 06:51:31 -0700

    change

--- Client/src/Components/Analytics/AnaltyicView.tsx
+++ Client/src/Components/Analytics/AnaltyicView.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { gapi } from 'gapi-script';
+import { useEffect, useState } from "react";
+import { gapi } from "gapi-script";
 
 function AnalyticsView() {
   const [analyticsData, setAnalyticsData] = useState<any>(null);
@@ -8,54 +8,59 @@ function AnalyticsView() {
     const fetchData = async () => {
       try {
         // Initialize the gapi client with your Google Analytics 4 property ID
-        gapi.load('client', () => {
+        gapi.load("client", () => {
           // Initialize the Google API client
-          gapi.client.init({
-            apiKey: 'AIzaSyCTaDImAZu8RW7Sv5d3TUg4hyqr6zeElNo',
-            clientId: '998694842602-ofo43avidduql3k6p2mmka7k568bquvo.apps.googleusercontent.com',
-            discoveryDocs: ['https://analyticsreporting.googleapis.com/$discovery/rest?version=v4'],
-            plugin_name: 'PLUGIN',
-            scope: 'https://www.googleapis.com/auth/analytics.readonly',
-          }).then(() => {
-            // Make a request to the Google Analytics Data API
-            gapi.client.analyticsreporting.reports.batchGet({
-              requestBody: {
-                reportRequests: [
-                  {
-                    viewId: 'G-9LNPEENX44',
-                    dateRanges: [
+          gapi.client
+            .init({
+              apiKey: "AIzaSyCTaDImAZu8RW7Sv5d3TUg4hyqr6zeElNo",
+              clientId:
+                "998694842602-ofo43avidduql3k6p2mmka7k568bquvo.apps.googleusercontent.com",
+              discoveryDocs: [
+                "https://analyticsreporting.googleapis.com/$discovery/rest?version=v4",
+              ],
+              plugin_name: "PLUGIN",
+              scope: "https://www.googleapis.com/auth/analytics.readonly",
+            })
+            .then(() => {
+              // Make a request to the Google Analytics Data API
+              gapi.client.analyticsreporting.reports
+                .batchGet({
+                  requestBody: {
+                    reportRequests: [
                       {
-                        startDate: '30daysAgo',
-                        endDate: 'today',
+                        viewId: "G-9LNPEENX44",
+                        dateRanges: [
+                          {
+                            startDate: "30daysAgo",
+                            endDate: "today",
+                          },
+                        ],
+                        metrics: [
+                          { expression: "ga:users" },
+                          { expression: "ga:sessions" },
+                        ],
                       },
                     ],
-                    metrics: [
-                      { expression: 'ga:users' },
-                      { expression: 'ga:sessions' },
-                    ],
                   },
-                ],
-              },
-            }).then((response:any) => {
-              // Set the analytics data in state
-              console.log(response)
-              setAnalyticsData(response.result.reports[0].data);
-            }).catch((error:any) => {
-              console.error('Error fetching analytics data:', error);
-              setAnalyticsData(analyticsData);
+                })
+                .then((response: any) => {
+                  // Set the analytics data in state
+                  console.log(response);
+                  setAnalyticsData(response.result.reports[0].data);
+                })
+                .catch((error: any) => {
+                  console.error("Error fetching analytics data:", error);
+                  setAnalyticsData(analyticsData);
+                });
             });
-          });
         });
-        
       } catch (error) {
-        console.error('Error fetching analytics data:', error);
-        
+        console.error("Error fetching analytics data:", error);
       }
     };
-  
+
     fetchData();
   }, [analyticsData]);
-  
 }
 
 export default AnalyticsView;
